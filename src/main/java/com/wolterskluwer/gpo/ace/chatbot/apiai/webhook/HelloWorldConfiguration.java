67e2e3e4d81c5ac16caa8d35bef7c3a5a7262eb0
Robert Istvan <robert_istvan@epam.com>
2017-03-18 17:38:20 -0400

    Initial draft.

---
+++ src/main/java/com/wolterskluwer/gpo/ace/chatbot/apiai/webhook/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.wolterskluwer.gpo.ace.chatbot.apiai.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldConfiguration.class, args);
+	}
+
+}
